4a3fb2958f2cbb81aa0699acd455c7d0925b9a70
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2024-05-21 23:17:59 +0600

    createAProduct api implemented

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
+import { ProductRoutes } from './modules/products/product.route';
 const app = express();
 
+// parsers
+app.use(express.json());
+app.use('/api/products',ProductRoutes);
+
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!!!')
+  res.send('Maassalam!!!')
 })
 
 export default app;
---
+++ src/modules/products/product.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { ProductServices } from "./product.service";
+
+const createProduct = async(req:Request, res:Response)=>{
+    const productData = req.body;
+    const result = await ProductServices.createProduct(productData);
+
+    res.json({
+        success:true,
+        message:"Product created successfully",
+        data:result,
+    })
+}
+
+export const ProductController = {
+    createProduct,
+}
