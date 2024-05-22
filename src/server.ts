7f52f4da3136b1918374f4dea4d1709a8c598bb8
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-05-22 09:35:36 +0600

    console message qualified

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ async function main() {
 
 
     app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`)
+        console.log(`L2B3A2 app listening on port ${config.port}`)
       })
     } catch (err) {
         console.log(err);
