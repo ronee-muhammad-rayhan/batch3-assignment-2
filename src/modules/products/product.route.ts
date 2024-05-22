6b6f82a05da95ff503587475fad33c7aa07b890e
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-05-22 11:33:48 +0600

    getAProductById

--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/", ProductController.createProduct);
 router.get("/", ProductController.getAllProducts);
-router.get("/", ProductController.getAllProducts);
+router.get("/:productId", ProductController.getProductById);
 router.put("/:productId", ProductController.updateProductById);
 router.delete("/:productId", ProductController.deleteAProduct);
 
