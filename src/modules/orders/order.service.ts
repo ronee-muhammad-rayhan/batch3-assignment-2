2c07394979d30624be2acd717dd40fd94722590a
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2024-05-22 16:22:44 +0600

    Inventory Management Logic implemented

--- src/modules/orders/order.service.ts
+++ src/modules/orders/order.service.ts
@@ -1,9 +1,56 @@
+import { Product } from "../products/product.model";
 import { TOrder } from "./order.interface";
 import Order from "./order.model";
 
 const createOrder = async (payload: TOrder) => {
-  const result = await Order.create(payload);
-  return result;
+  // console.log(payload);
+
+  const productId = payload.productId;
+
+  const productOld = await Product.findOne({ _id: productId });
+
+  console.log(productOld);
+
+
+  if(productOld?.inventory?.inStock && productOld?.inventory?.quantity-payload?.quantity>=0) {
+      const newQuantity = productOld.inventory.quantity - payload.quantity;
+      console.log(newQuantity);
+      const result = await Order.create(payload);
+      await Product.findByIdAndUpdate(productId, {"inventory.quantity": newQuantity});
+      return result;
+    }
+
+
+
+  // if(productOld?.inventory?.inStock && productOld?.inventory?.quantity-payload?.quantity>=0) {
+  //   const price = productOld.price * payload.quantity;
+  //   const result = await Order.create({
+  //    ...payload,
+  //     price,
+  //   });
+  //   await Product.findByIdAndUpdate(productId, {"inventory.quantity": productOld?.inventory?.quantity-payload?.quantity});
+  //   return result;
+  // }
+
+
+  // if (!productOld) {
+  //   throw new Error("Product not found");
+  // } else {  
+
+  // await Product.findByIdAndUpdate(productId, {"inventory.quantity": payload.quantity});
+  // const productNew = await Product.findOne({ _id: productId });
+
+  // const productNew = await Product.findOne({ _id: productId });
+  // const price = productOld.price * payload.quantity;
+  // const result = await Order.create({
+  //  ...payload,
+  //   price,
+  // });
+  // return result;
+
+  // const result = await Order.create(payload);
+
+  // return result;
 };
 
 const retrieveAllOrders = async (queryEmail:any) => {
